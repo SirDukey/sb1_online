b72a59acac03ec68c728a2b774e4c7cc1bc22a84
Michael Duke <42153017+SirDukey@users.noreply.github.com>
2018-11-06 13:24:29 +0200

    Update sb1_check.py

--- sb1_check.py
+++ sb1_check.py
@@ -1,26 +1,12 @@
-import paramiko
-
-client = paramiko.client.SSHClient()
-client.load_system_host_keys()
-
 try:
-    client.connect('hostname', port=, username='', password='')
-    try:
-        stdin, stdout, stderr = client.exec_command('cat sysadmin/online.txt', timeout=5)
-        if stdout:
-            x = stdout.read()
-            x = x.decode('ascii')
-            if x == 'online\n':
+    with open('/root/sysadmin/online.txt', 'r') as f:
+        try:
+            res = f.read()
+            if res == 'online\n':
                 print(0)
-        elif stderr:
+        except:
             print(1)
 
-    except:
-        '''cant read online.txt file'''
-        print(2)
-
-    finally:
-        client.close()
 except:
-    '''cant connect'''
-    print(3)
+    '''cant read online.txt file'''
+    print(2)
